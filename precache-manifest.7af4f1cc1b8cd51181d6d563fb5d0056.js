43a24641fd28edc06f0728ded83f004ae68ff63a
Jonas Voetmann <48560462+JonasVoet@users.noreply.github.com>
2019-09-27 17:23:45 +0200

    Whole project

---
+++ precache-manifest.7af4f1cc1b8cd51181d6d563fb5d0056.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d4f94b14f7667d216574566551898c",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "f1d786f8d79fd04565bd",
+    "url": "/tictactoe/static/css/main.f9a6d002.chunk.css"
+  },
+  {
+    "revision": "b0f0e6a568de94b9f6d1",
+    "url": "/tictactoe/static/js/2.3ae04c45.chunk.js"
+  },
+  {
+    "revision": "f1d786f8d79fd04565bd",
+    "url": "/tictactoe/static/js/main.dee4f2be.chunk.js"
+  },
+  {
+    "revision": "c76aecb9e6aa6037cd21",
+    "url": "/tictactoe/static/js/runtime-main.6e4b23ef.js"
+  }
+]);
